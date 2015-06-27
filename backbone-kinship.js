8960b17fd6f82bd3ab85b465fb3c7c673cd2d444
Gustaf Forsslund <gustaf.forsslund@oktavilla.se>
2015-06-27 14:21:43 +0200

    Adds native Backbone event handling
    Fixes https://github.com/Oktavilla/backbone-kinship/issues/8
    Fixes https://github.com/Oktavilla/backbone-kinship/issues/7 (as an
    automatic side-effect, doing it otherwise would be complicated)

--- backbone-kinship.js
+++ backbone-kinship.js
@@ -7,7 +7,7 @@
 * }
 * After that the related collection/model instances are available both directly on the
 * model (Model.my-first-relation) and by using Model.get (Model.get("my-first-relation")).
-* Note that relations are undefined until they have been assigned a value.
+* Note that model relations are undefined until they have been assigned a value.
 */
 (function(root, factory) {
   "use strict";
@@ -23,16 +23,16 @@
 	else {
 		factory(root, root.Backbone, root._);
 	}
-} (this, function(exports, Backbone, _) {
+} (this, function (exports, Backbone, _) {
   Backbone.RelationalModel = Backbone.Model.extend({
     relations: {},
 
-    get: function(key) {
+    get: function (key) {
       return this.relations[key] ? this[key] : Backbone.Model.prototype.get.call(this, key);
     },
 
-    set: function(key, value, options) {
-      var attributes, isCloned;
+    set: function (key, value, options) {
+      var returnValue;
       // Handling the two function signatures, either key-value or attribute object
       if (_.isObject(key)) {
         attributes = key;
@@ -40,116 +40,75 @@
       } else {
         (attributes = {})[key] = value;
       }
-      // Initializing relational functionality (if it's not already done)
-      if (!this._attributes) {
-        // Setting up internal getter and setter for attributes to keep it clean
-        // from the actual releated models and collections
-        this._attributes = _.clone(this.attributes);
-        Object.defineProperty(this, "attributes", {
-          configurable: true,
-          enumerable: true,
-          get: function() {
-            return getAttributes(this);
-          },
-          set: function(value) {
-            return setAttributes(this, value);
-          }
-        });
-      }
       // Checking if any relations are among the attributes
-      _.each(this.relations, function(constructor, name) {
-        var entity;
-        var setMethod = "set";
-        // Making sure that this relation is set up
+      _.each(this.relations, function (constructor, name) {
+        var object;
+        // Setting up relational functionality for this attribute (if it's not already done)
         if (!this[name]) {
-          // Setting up new relation
-          entity = new constructor();
+          object = new constructor();
           // Only collections can be set up without data
-          if (typeof attributes[name] !== "undefined" || entity instanceof Backbone.Collection) {
-            this[name] = entity;
-            // Setting up dispatcher for relational events, for example that "add" on a relation called "collection" would trigger the event "collection:add" on this model
-            delegateEvents(this[name], this, name);
-          }
-          // Initial sets are always resets (if possible and unless required otherwise)
-          if ((!options || typeof options.reset === "undefined") && entity.reset) {
-            setMethod = "reset";
+          if (typeof attributes[name] !== "undefined" || object instanceof Backbone.Collection) {
+            this[name] = object;
+            // Setting up getter and setter for this attribute (so that it can be expanded)
+            defineRelatedAttribute(this, name, object);
+            // Setting up dispatcher for relational events
+            delegateEvents(object, this, name);
           }
         }
-        // Checking if there is data for this relation
-        if (typeof attributes[name] !== "undefined") {
-          // Switching to reset if required (and possible)
-          if (options && options.reset && this[name].reset) {
-            setMethod = "reset";
-          }
-          // Handling both key-value and attribute signatures
-          if (_.isObject(attributes[name])) {
-            this[name][setMethod](attributes[name], options);
-          } else {
-            this[name][setMethod](attributes[name], attributes[name].value, options);
-          }
-          // Removing relational data so it isn't also set on the model
-          if (key[name]) {
-             // Cloning to avoid affecting other users of this object (but we don't want to clone it more than once because performace)
-            if (!isCloned) {
-              key = _.clone(key);
-              isCloned = true;
-            }
-            delete key[name]; // Deleting from attribute object
-          } else {
-            key = null;
-          }
+        if (name in attributes) {
+          // "Propagating" set options
+          this[name]._setOptions = options;
         }
       }, this);
-      if (key) {
-        Backbone.Model.prototype.set.call(this, key, value, options);
+      // Letting Backbone do its real set magic
+      returnValue = Backbone.Model.prototype.set.call(this, key, value, options);
+      // Unsetting means that all attributes get wiped, including our attribute
+      // relations, so we handle that.
+      if (options && options.unset) {
+        _.each(this.relations, function (constructor, name) {
+          if (name in attributes && this[name]) {
+            if (this[name] instanceof Backbone.Collection) {
+              // Reconnecting unset collections (since an unset collection attribute is just an empty collection)
+              defineRelatedAttribute(this, name, this[name]);
+            } else {
+              // Removing others unset relations
+              delete this[name];
+            }
+          }
+        }, this);
       }
-      return this;
+      return returnValue;
     }
   });
 
   /**
-  * These events should also trigger a "change" event.
+  * Connects a model attribute to a related model or collection
+  * @param model The model to add the connection to
+  * @param name The name of attribute
+  * @param object The backbone object to connect
   */
-  var CHANGE_EVENTS = {
-    add: true,
-    remove: true,
-    change: true,
-    reset: true
-  };
-
-  /**
-  * Returns all attributes for a model
-  * Relational attributes will be JSONified
-  * @param model A Backbone-kinship Model
-  * @return Model attributes
-  */
-  function getAttributes(model) {
-    var attributes = model._attributes;
-    // Adding expanded json data from all relations
-    _.each(model.relations, function(constructor, name) {
-      if (model[name]) {
-        attributes[name] = model[name].toJSON();
+  function defineRelatedAttribute(model, name, object) {
+    Object.defineProperty(model.attributes, name, {
+      configurable: true,
+      enumerable: true,
+      get: function () {
+        return object.toJSON();
+      },
+      set: function (value) {
+        var options = object._setOptions;
+        var method = options && options.reset && object.reset ? "reset" : "set";
+        delete object._setOptions;
+        return object[method](value, options);
       }
     });
-    return attributes;
   }
-  
+
   /**
-  * Sets attributes for a model
-  * @param model A Backbone-kinship Model
-  * @param value The value to set
-  */
-  function setAttributes(model, value) {
-    model._attributes = value;
-  }
-  
-  /*
   * Delegates all events from one entity to another, on the format
-  * eventName:fromName. A "change" event will also be triggered for "add", "remove",
-  * "change" and "reset" events.
+  * eventName:fromName.
   * @param from The entity that emits the event
   * @param to The entity that should receive the event
-  * @param fromName Display name to use to show where this event originated
+  * @param fromName Display name to use for showing where this event originated
   */
   function delegateEvents(from, to, fromName) {
     from.bind("all", function() {
@@ -157,10 +116,6 @@
       var eventName = args[0];
       args[0] = eventName + ":" + fromName;
       to.trigger.apply(to, args);
-      if (CHANGE_EVENTS[eventName]) {
-        args[0] = "change";
-        to.trigger.apply(to, args);
-      }
     });
   }
 }));
